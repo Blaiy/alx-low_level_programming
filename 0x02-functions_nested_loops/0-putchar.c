20e808f8f8dc736223009f0cbf1a923ed3a41dca
Blaiy <annabelblessing02@gmail.com>
2022-12-14 02:38:16 -0800

    0putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
   * main - Entry point
-  * Description:prints '_putchar\n'
+  * Description:prints '_putchar'
   * Return: Always 0 (success)
   */
 int main(void)
 {
-	char c[9] = "_putchar\n";
+	char c[9] = "_putchar";
 	int i = 0;
 
 	for (i = 0; i < 8; i++)
 	{
 		_putchar(c[i]);
 	}
+	_putchar('\n');
+
 	return (0);
 }
